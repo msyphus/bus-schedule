448a705a342e147bb0ce4d4761f32accf2e5ecbf
msyphus <mark.syphus@gmail.com>
2020-04-06 23:06:36 -0600

    calculated next arrival and minutes left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,11 +6,22 @@ $("button").on("click", function() {
     var frequency = $("#freq").val();
     var firstBus = $("#frst").val();
 
-database.ref().push({
-    inputRoute: route,
-    inputDest: destination,
-    inputFreq: frequency,
-    inputFirstBus: firstBus
+    //Variables for time calculations
+    var firstBusConverted = moment(firstBus, "HH:mm").subtract(1, "years");
+    var timeDifference = moment().diff(moment(firstBusConverted), "minutes");
+    var timeRemainder = timeDifference % frequency;
+    var minutesLeft = frequency - timeRemainder
+    var nextBus = moment().add(minutesLeft, "minutes");
+    var nextBusFormatted = moment(nextBus).format("HH:mm");
+    
+
+    database.ref().push({
+        inputRoute: route,
+        inputDest: destination,
+        inputFreq: frequency,
+        inputFirstBus: firstBus,
+        inputNextBus: nextBusFormatted,
+        inputMinLeft: minutesLeft
     })
 });
 
@@ -18,14 +29,15 @@ database.ref().on("child_added", function(snapshot) {
     var addedRoute = snapshot.val().inputRoute;
     var addedDest = snapshot.val().inputDest;
     var addedFreq = snapshot.val().inputFreq;
-    var addedFirstBus = snapshot.val().inputFirstBus;
+    var addedNextBus = snapshot.val().inputNextBus;
+    var addedMinLeft = snapshot.val().inputMinLeft;
 
 var newRow = $("<tr>").append(
     $("<td>").text(addedRoute),
     $("<td>").text(addedDest),
     $("<td>").text(addedFreq),
-    //$("<td>").text(nextArrival),
-    //$("<td>").text(minLeft)
+    $("<td>").text(addedNextBus),
+    $("<td>").text(addedMinLeft)
 );
 
 $("#routeTable").append(newRow);
