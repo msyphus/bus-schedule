b01dec8215d941011710caf0e9e5672a3ba52292
msyphus <mark.syphus@gmail.com>
2020-04-08 23:59:34 -0600

    made minutes left calculation update every minute

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,40 +6,38 @@ $("button").on("click", function() {
     var frequency = $("#freq").val();
     var firstBus = $("#frst").val();
 
-    //Variables for time calculations
-    var firstBusConverted = moment(firstBus, "HH:mm").subtract(1, "years");
-    var timeDifference = moment().diff(moment(firstBusConverted), "minutes");
-    var timeRemainder = timeDifference % frequency;
-    var minutesLeft = frequency - timeRemainder
-    var nextBus = moment().add(minutesLeft, "minutes");
-    var nextBusFormatted = moment(nextBus).format("HH:mm");
-    
-
     database.ref().push({
         inputRoute: route,
         inputDest: destination,
         inputFreq: frequency,
         inputFirstBus: firstBus,
-        inputNextBus: nextBusFormatted,
-        inputMinLeft: minutesLeft
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     })
 });
 
 database.ref().on("child_added", function(snapshot) {
+    var firstBusConverted = moment(snapshot.val().inputFirstBus, "HH:mm").subtract(1, "years");
+    var timeDifference = moment().diff(moment(firstBusConverted), "minutes");
+    var timeRemainder = timeDifference % snapshot.val().inputFreq;
+    var minutesLeft = snapshot.val().inputFreq - timeRemainder
+    var nextBus = moment().add(minutesLeft, "minutes");
+    var nextBusFormatted = moment(nextBus).format("HH:mm");
+
     var addedRoute = snapshot.val().inputRoute;
     var addedDest = snapshot.val().inputDest;
     var addedFreq = snapshot.val().inputFreq;
-    var addedNextBus = snapshot.val().inputNextBus;
-    var addedMinLeft = snapshot.val().inputMinLeft;
 
 var newRow = $("<tr>").append(
     $("<td>").text(addedRoute),
     $("<td>").text(addedDest),
     $("<td>").text(addedFreq),
-    $("<td>").text(addedNextBus),
-    $("<td>").text(addedMinLeft)
+    $("<td>").text(nextBusFormatted),
+    $("<td>").text(minutesLeft)
 );
 
 $("#routeTable").append(newRow);
 });
 
+setInterval(function () {
+    window.location.reload();
+}, 60000);
